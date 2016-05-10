d5cae4621b45fdd848994f120aed2aa971d1c795
Pavel Kuzmenko <kyzmenko.pavel@gmail.com>
2016-05-10 22:30:08 +0300

    fix segfal

--- src/Params.cpp
+++ src/Params.cpp
@@ -140,6 +140,10 @@ Params &Params::parse()
             {
                 boost::algorithm::to_lower(retargeting);
                 boost::split(retargeting_offers_, retargeting, boost::is_any_of(";"));
+                if (retargeting_offers_.size()> 500)
+                {
+                    retargeting_offers_.erase(retargeting_offers_.begin()+499, retargeting_offers_.end());
+                }
             }
             for (auto i=retargeting_offers_.begin(); i != retargeting_offers_.end() ; ++i)
             {
